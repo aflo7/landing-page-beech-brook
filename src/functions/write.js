69216e97085a5d40f872dcfb3a233bb705571023
Andres Flores <andresflo883@gmail.com>
2023-12-19 22:00:10 -0500

    added meta env console log

--- src/functions/write.js
+++ src/functions/write.js
@@ -1,6 +1,8 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { PutCommand, DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 
+console.log(import.meta.env)
+
 const client = new DynamoDBClient({
   region: 'us-east-2',
   credentials: {
